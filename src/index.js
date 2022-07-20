d786f0816acc8d3563f6ca7e761223dd22cd4000
Valeriya <chva5000@gmail.com>
2022-07-21 02:30:37 +0300

    feat: added Authentication

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import './index.css';
 
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+} from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD4n7V9q2f7FcDfE-rMMGglGwV-uhrklU4',
+  authDomain: 'rickandmorty-5b1a1.firebaseapp.com',
+  projectId: 'rickandmorty-5b1a1',
+  storageBucket: 'rickandmorty-5b1a1.appspot.com',
+  messagingSenderId: '1042432215783',
+  appId: '1:1042432215783:web:710933441cb8d249aaf042',
+  measurementId: 'G-S6CBEKY0SX'
+};
+
+const app = initializeApp(firebaseConfig);
+
+const ContextState = {
+  auth: null
+};
+
+export const Context = createContext<ContextState>({ auth: null });
+const auth = getAuth(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Context.Provider value={{ auth }}>
+      <App />
+    </Context.Provider>
+  </React.StrictMode>,
 );
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+} from 'firebase/auth';
+
+import { Context } from '../../index';
 
 const Login = () => {
-  return <h1>login</h1>;
+  const { auth } = useContext(Context);
+
+  const login = async () => {
+    const googleProvider = new GoogleAuthProvider();
+
+    const res = await signInWithPopup(auth, googleProvider);
+    const { user } = res;
+    console.log(user);
+
+  };
+
+  return (
+    <>
+      <button type="button" onClick={login}>login button</button>
+      Login
+    </>
+  );
 };
+
 export default Login;
